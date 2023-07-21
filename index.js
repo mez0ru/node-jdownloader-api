962cb06f503a080eeec032a15d995a04b8c9cd20
Hamzah Al-washali <32926868+mez0ru@users.noreply.github.com>
2023-07-21 15:39:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -218,8 +218,8 @@ exports.getDirectConnectionInfos = deviceId => new Promise((resolve, rejected) =
 });
 
 exports.addLinks = (links, deviceId, autostart, packageName = undefined, destinationFolder = undefined) => {
-  const packageNameParam = packageName ? `,"packageName": "${packageName}"` : '';
-  const destinationParam = destinationFolder ? `,"destinationFolder": "${destinationFolder}"` : '';
+  const packageNameParam = packageName ? `,"packageName":"${packageName}"` : '';
+  const destinationParam = destinationFolder ? `,"destinationFolder":"${destinationFolder}"` : '';
   const params = `{"priority":"DEFAULT"${destinationParam},"links":"${links}","autostart":${autostart}${packageNameParam}}`;
   return new Promise((resolve, rejected) => {
     callAction('/linkgrabberv2/addLinks', deviceId, [params])
